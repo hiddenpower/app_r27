a0d0232eaab2bac2c0900ddf9ccba5b9c1ca54aa
Gustavo Sandoval <gusxsandoval@gmail.com>
2013-05-16 11:15:50 -0500

    git test

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AppR27::Application.config.secret_token = 'b2363c8de430a5554791003cf2ff2eff27ba69f8e69efda66e9b01e5a7c7ac6e33e7c4384b9984b14384860c4401c62ee3b841c73389b3400f80510716d5d9f4'
--- db/migrate/20130506215032_add_status_to_clients.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusToClients < ActiveRecord::Migration
-  def change
-    add_column :clients, :status, :string
-    add_column :clients, :date, :date
-  end
-end
